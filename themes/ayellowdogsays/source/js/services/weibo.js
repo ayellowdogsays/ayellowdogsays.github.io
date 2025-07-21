46db119d6bc5e878aab4e1a0a82e225e32409fd3
ayellowdogsays <ahuangsays@outlook.com>
2025-07-21 16:23:30 +0800

    s

--- themes/ayellowdogsays/source/js/services/weibo.js
+++ themes/ayellowdogsays/source/js/services/weibo.js
@@ -3,14 +3,13 @@ utils.jq(() => {
     const els = document.getElementsByClassName('ds-weibo');
     for (var i = 0; i < els.length; i++) {
       const el = els[i];
-      const api = el.dataset.api;
+      const api = el.getAttribute('api');
       if (api == null) {
         continue;
       }
       const default_avatar = el.getAttribute('avatar') || def.avatar;
       // layout
-      utils.request(el, api, async resp => {
-        const data = await resp.json();
+      utils.request(el, api, function(data) {
         const arr = data.tweets || [];
         const limit = el.getAttribute('limit');
         arr.forEach((item, i) => {
